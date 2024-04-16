289afaa6632fd02c98d9082f6b602f622b3e6efb
Italo Valcy <italo@ampath.net>
2024-04-16 08:24:53 -0300

    adding Firewall and method to config IP addresses on links

--- mnsec/net.py
+++ mnsec/net.py
@@ -19,6 +19,7 @@ from mininet.net import Mininet, MininetWithControlNet
 
 import mnsec.apps.all
 from mnsec.apps.app_manager import AppManager
+from mnsec.nodelib import IPTablesFirewall
 
 
 VERSION = "0.1.0"
@@ -65,6 +66,41 @@ class Mininet_sec(Mininet):
         for cleanup in self.cleanups:
             cleanup()
         Mininet.stop(self)
+
+    def addFirewall( self, name='fw0', **params):
+        """Add a Firewall to the Mininet-Sec network
+           name: name of Firewall node
+           params: other Firewall node params, notably:
+               rules_v4: string, filename or dict(list) with IPv4 rules
+               rules_v6: string, filename or dict(list) with IPv6 rules"""
+        fw = self.addHost( name, cls=IPTablesFirewall, **params )
+        return fw
+
+    def addLink(
+        self, node1, node2, ipv4_node1=None, ipv4_node2=None,
+        ipv6_node1=None, ipv6_node2=None, **params
+    ):
+        """"Add a link from node1 to node2 and configure IP addr
+            node1: source node (or name)
+            node2: dest node (or name)
+            ipv4_node1: IPv4 address to configure on node1
+            ipv4_node2: IPv4 address to configure on node2
+            ipv6_node1: IPv6 address to configure on node1
+            ipv6_node2: IPv6 address to configure on node2
+            params: additional link params (optional)
+            returns: link object"""
+        link = Mininet.addLink(self, node1, node2, **params)
+
+        if ipv4_node1:
+            link.intf1.setIP(ipv4_node1)
+        if ipv4_node2:
+            link.intf2.setIP(ipv4_node2)
+        if ipv6_node1:
+            link.intf1.setIP(ipv6_node1)
+        if ipv6_node2:
+            link.intf2.setIP(ipv6_node2)
+
+        return link
 
 class MininetSecWithControlNet(MininetWithControlNet):
     """Control network support."""
---
+++ mnsec/nodelib.py
@@ -0,0 +1,96 @@
+"""
+Node objects for Mininet-Sec
+
+LinuxServer: provides minimal functionatility to work as a server
+"""
+import os
+import re
+import traceback
+
+from mininet.node import Node
+from mininet.log import info, error, warn, debug
+
+
+class IPTablesFirewall( Node ):
+    "A Node with IPTables Linux Firewall."
+
+    def __init__( self, name, rules_v4="", rules_v6="", **params):
+        """Start IPTables firewall
+            rules_v4: IPv4 firewall rules to be loaded into iptables via
+                iptables-restore command. Three formats are accepted:
+                    - filename containing the IPv4 rules (from iptables-save)
+                    - multiline string representing IPv4 rules (iptables-save)
+                    - dict(list) where keys are tables and values are list of
+                      chains or rules (same format as iptables-save)
+            rules_v6: IPv6 firewall rules to be loaded into ip6tables via
+                ip6tables-restore command. Same format as rules_v4
+            flush: flush iptables before installing NAT rules"""
+        Node.__init__( self, name, **params )
+
+        self.rules_v4 = rules_v4
+        self.rules_v6 = rules_v6
+
+        self.fw_rules_v4 = f"self.params['homeDir']/iptables_v4.conf"
+        if isinstance(self.rules_v4, dict):
+            f = open(self.fw_rules_v4, "w")
+            for table in self.rules_v4:
+                f.write(f"*{table}\n")
+                for rule in self.rules_v4[table]:
+                    f.write(f"*{rule}\n")
+                f.write("COMMIT\n\n")
+            f.close()
+        elif isinstance(self.rules_v4, str) and os.path.isfile(self.rules_v4):
+            self.fw_rules_v4 = self.rules_v4
+        else:
+            with open(self.fw_rules_v4, "w") as f:
+                f.write(self.rules_v4)
+
+        self.fw_rules_v6 = f"self.params['homeDir']/iptables_v6.conf"
+        if isinstance(self.rules_v6, dict):
+            f = open(self.fw_rules_v6, "w")
+            for table in self.rules_v6:
+                f.write(f"*{table}\n")
+                for rule in self.rules_v6[table]:
+                    f.write(f"*{rule}\n")
+                f.write("COMMIT\n\n")
+            f.close()
+        elif isinstance(self.rules_v6, str) and os.path.isfile(self.rules_v6):
+            self.fw_rules_v6 = self.rules_v6
+        else:
+            with open(self.fw_rules_v6, "w") as f:
+                f.write(self.rules_v6)
+
+    def start( self, **moreParams ):
+        self.params.update(moreParams)
+
+        # Default policy DROP
+        for command in ["iptables", "ip6tables"]:
+            self.cmd("f{command} -t filter -P INPUT DROP")
+            self.cmd("f{command} -t filter -P FORWARD DROP")
+            self.cmd("f{command} -t filter -P OUTPUT DROP")
+
+        # Load firewall rules
+        self.cmd(f"iptables-restore < {self.fw_rules_v4}")
+        self.cmd(f"ip6tables-restore < {self.fw_rules_v6}")
+
+        # Enable forwarding
+        self.cmd("sysctl net.ipv4.ip_forward=1")
+        self.cmd("sysctl net.ipv6.conf.all.forwarding=1")
+
+    def terminate( self ):
+        self.cmd("sysctl net.ipv4.ip_forward=0")
+        self.cmd("sysctl net.ipv6.conf.all.forwarding=0")
+        for command in ["iptables", "ip6tables"]:
+            self.cmd("f{command} -t filter -P INPUT ACCEPT")
+            self.cmd("f{command} -t filter -P FORWARD ACCEPT")
+            self.cmd("f{command} -t filter -P OUTPUT ACCEPT")
+            self.cmd("f{command} -t filter -F")
+            self.cmd("f{command} -t filter -X")
+            self.cmd("f{command} -t filter -Z ")
+            self.cmd("f{command} -t nat -F")
+            self.cmd("f{command} -t nat -X")
+            self.cmd("f{command} -t mangle -F")
+            self.cmd("f{command} -t mangle -X")
+            self.cmd("f{command} -t raw -F")
+            self.cmd("f{command} -t raw -X")
+        Node.terminate(self)
